e9fff4cdc76794ea21a305fdbd1b6c87bc356661
rafirachmadramadhan <rafirachmadramadhan@gmail.com>
2022-12-10 17:54:35 +0700

    Add files via upload
    new image, font, and design web

---
+++ Documents/test_capstone/static/js/reveal.js
@@ -0,0 +1,21 @@
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = -250;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+
+window.addEventListener('scroll', this.reveal, false);
+window.addEventListener('mausemove', this.reveal, false);
+window.addEventListener('load', this.reveal, false);
+
